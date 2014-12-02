5ce699cb1fa743f6ee017194978c5ea7feff9150
Alejandro Seguí <alesegdia@gmail.com>
2014-12-02 21:12:57 +0100

    Texture from camera frame dumped to irrlicht texture and placed as background.

--- cmake/FindOpenCV.cmake
+++ cmake/FindOpenCV.cmake
@@ -1,242 +1,243 @@
 # - Try to find OpenCV library installation
 # See http://sourceforge.net/projects/opencvlibrary/
 #
-# The follwoing variables are optionally searched for defaults
-#  OpenCV_ROOT_DIR:            Base directory of OpenCv tree to use.
-#  OpenCV_FIND_REQUIRED_COMPONENTS : FIND_PACKAGE(OpenCV COMPONENTS ..) 
-#    compatible interface. typically  CV CXCORE CVAUX HIGHGUI CVCAM .. etc.
+# The following variable is optionally searched for defaults
+#  OPENCV_ROOT_DIR:            Base directory of OpenCv tree to use.
 #
-# The following are set after configuration is done: 
-#  OpenCV_FOUND
-#  OpenCV_INCLUDE_DIR
-#  OpenCV_LIBRARIES
-#  OpenCV_LINK_DIRECTORIES
+#  OPENCV_NEW_LIBRARY_NAMES   Set to YES before searching if you want to
+# The following are set after configuration is done:
+#  OPENCV_FOUND
+#  OPENCV_INCLUDE_DIRS
+#  OPENCV_LIBRARIES
 #
-# 2004/05 Jan Woetzel, Friso, Daniel Grest 
+# 2004/05 Jan Woetzel, Friso, Daniel Grest
 # 2006/01 complete rewrite by Jan Woetzel
-# 1006/09 2nd rewrite introducing ROOT_DIR and PATH_SUFFIXES 
+# 2006/09 2nd rewrite introducing ROOT_DIR and PATH_SUFFIXES
 #   to handle multiple installed versions gracefully by Jan Woetzel
+# 2010/02 Ryan Pavlik (Iowa State University) - partial rewrite to standardize
 #
 # tested with:
 # -OpenCV 0.97 (beta5a):  MSVS 7.1, gcc 3.3, gcc 4.1
 # -OpenCV 0.99 (1.0rc1):  MSVS 7.1
 #
 # www.mip.informatik.uni-kiel.de/~jw
-#
-# ---------------------
-#
-# $Id: FindOpenCV.cmake 8 2009-01-04 21:13:48Z adeguet1 $
-# ERC-CISST version:
-#  
-# - Removed deprecated code starting with cap. OPENCV
-# - Removed debugging code and messages
-# - Removed path and options specifics to previous authors setups
-#
-# This file should be removed when CMake will provide an equivalent
+# academic.cleardefinition.com
+# --------------------------------
 
+set(OPENCV_ROOT_DIR
+	"${OPENCV_ROOT_DIR}"
+	CACHE
+	PATH
+	"Path to search for OpenCV")
 
-# required cv components with header and library if COMPONENTS unspecified
-IF(NOT OpenCV_FIND_COMPONENTS)
-  # default
+find_package(OpenCV QUIET NO_MODULE)
+if(OpenCV_LIBS AND NOT OpenCV_LIBRARIES)
+	set(OPENCV_LIBRARIES ${OpenCV_LIBS})
+	set(OPENCV_FOUND true)
 
+	# typical root dirs of installations, exactly one of them is used
+	program_files_glob(_dirs "/OpenCV*/")
 
-
-  #
-  # fix for OpenCV 2.4.6.1 on Mac OSX
-  #
-  #SET(OpenCV_FIND_REQUIRED_COMPONENTS CV CXCORE HIGHGUI)
-  SET(OpenCV_FIND_REQUIRED_COMPONENTS CORE HIGHGUI)
-
-
-
-#  IF(WIN32)
-#    LIST(APPEND OpenCV_FIND_REQUIRED_COMPONENTS  CVCAM ) # WIN32 only actually
-#  ENDIF(WIN32)  
-ENDIF(NOT OpenCV_FIND_COMPONENTS)
-
-
-# typical root dirs of installations, exactly one of them is used
-SET(OpenCV_POSSIBLE_ROOT_DIRS
-  "${OpenCV_ROOT_DIR}"
-  "$ENV{OpenCV_ROOT_DIR}"  
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Intel(R) Open Source Computer Vision Library_is1;Inno Setup: App Path]"
-  "$ENV{ProgramFiles}/OpenCV"
-  "/usr/local"
-  "/usr"
-	"/opt/local"
-  )
-  
-#
-# select exactly ONE OpenCV base directory/tree 
-# to avoid mixing different version headers and libs
-#
-FIND_PATH(OpenCV_ROOT_DIR 
-  NAMES 
-  cv/include/cv.h     # windows
-  include/opencv/cv.h # linux /opt/net
-  include/cv/cv.h 
-  include/cv.h 
-  PATHS ${OpenCV_POSSIBLE_ROOT_DIRS})
-
-# header include dir suffixes appended to OpenCV_ROOT_DIR
-SET(OpenCV_INCDIR_SUFFIXES
-  include
-  include/cv
-  include/opencv
-  cv/include
-  cxcore/include
-  cvaux/include
-  otherlibs/cvcam
-  otherlibs/highgui
-  )
-
-# library linkdir suffixes appended to OpenCV_ROOT_DIR 
-SET(OpenCV_LIBDIR_SUFFIXES
-  lib
-  OpenCV/lib
-  )
-
-# find incdir for each lib
-FIND_PATH(OpenCV_CV_INCLUDE_DIR
-  NAMES cv.h      
-  PATHS ${OpenCV_ROOT_DIR} 
-  PATH_SUFFIXES ${OpenCV_INCDIR_SUFFIXES})
-
-FIND_PATH(OpenCV_CXCORE_INCLUDE_DIR   
-  NAMES cxcore.h
-  PATHS ${OpenCV_ROOT_DIR} 
-  PATH_SUFFIXES ${OpenCV_INCDIR_SUFFIXES})
-FIND_PATH(OpenCV_CVAUX_INCLUDE_DIR    
-  NAMES cvaux.h
-  PATHS ${OpenCV_ROOT_DIR} 
-  PATH_SUFFIXES ${OpenCV_INCDIR_SUFFIXES})
-FIND_PATH(OpenCV_HIGHGUI_INCLUDE_DIR  
-  NAMES highgui.h 
-  PATHS ${OpenCV_ROOT_DIR} 
-  PATH_SUFFIXES ${OpenCV_INCDIR_SUFFIXES})
-FIND_PATH(OpenCV_CVCAM_INCLUDE_DIR    
-  NAMES cvcam.h 
-  PATHS ${OpenCV_ROOT_DIR} 
-  PATH_SUFFIXES ${OpenCV_INCDIR_SUFFIXES})
+	#
+	# select exactly ONE OPENCV base directory/tree
+	# to avoid mixing different version headers and libs
+	#
+	find_path(OPENCV_BASE_DIR
+		NAMES
+		cv/include/cv.h
+		include/opencv/cv.h
+		include/cv/cv.h
+		include/cv.h
+		HINTS
+		"${OPENCV_ROOT_DIR}"
+		"$ENV{OPENCV_ROOT_DIR}"
+		"[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Intel(R) Open Source Computer Vision Library_is1;Inno Setup: App Path]"
+		${_dirs})
 
 
 
-#
-# fix for OpenCV 2.4.6.1 on Mac OSX
-#
-FIND_PATH(OpenCV_CORE_INCLUDE_DIR   
-  NAMES cxcore.h
-  PATHS ${OpenCV_ROOT_DIR} 
-  PATH_SUFFIXES ${OpenCV_INCDIR_SUFFIXES})
+
+	# header include dir suffixes appended to OPENCV_BASE_DIR
+	set(OPENCV_INCDIR_SUFFIXES
+		include
+		include/cv
+		include/opencv
+		cv/include
+		cxcore/include
+		cvaux/include
+		otherlibs/cvcam/include
+		otherlibs/highgui
+		otherlibs/highgui/include
+		otherlibs/_graphics/include)
+
+	# library linkdir suffixes appended to OPENCV_BASE_DIR
+	set(OPENCV_LIBDIR_SUFFIXES
+		lib
+		lib64
+		OpenCV/lib
+		otherlibs/_graphics/lib)
 
 
+	#
+	# find incdir for each lib
+	#
+	find_path(OPENCV_cv_INCLUDE_DIR
+		NAMES
+		cv.h
+		HINTS
+		"${OPENCV_BASE_DIR}"
+		"${OPENCV_ROOT_DIR}"
+		PATH_SUFFIXES
+		${OPENCV_INCDIR_SUFFIXES})
+	find_path(OPENCV_cxcore_INCLUDE_DIR
+		NAMES
+		cxcore.h
+		HINTS
+		"${OPENCV_BASE_DIR}"
+		"${OPENCV_ROOT_DIR}"
+		PATH_SUFFIXES
+		${OPENCV_INCDIR_SUFFIXES})
+	find_path(OPENCV_cxcore_INCLUDE_DIR
+		NAMES
+		cvaux.h
+		HINTS
+		"${OPENCV_BASE_DIR}"
+		"${OPENCV_ROOT_DIR}"
+		PATH_SUFFIXES
+		${OPENCV_INCDIR_SUFFIXES})
+	find_path(OPENCV_highgui_INCLUDE_DIR
+		NAMES
+		highgui.h
+		HINTS
+		"${OPENCV_BASE_DIR}"
+		"${OPENCV_ROOT_DIR}"
+		PATH_SUFFIXES
+		${OPENCV_INCDIR_SUFFIXES})
+	find_path(OPENCV_cvcam_INCLUDE_DIR
+		NAMES
+		cvcam.h
+		HINTS
+		"${OPENCV_BASE_DIR}"
+		"${OPENCV_ROOT_DIR}"
+		PATH_SUFFIXES
+		${OPENCV_INCDIR_SUFFIXES})
 
-#
-# find sbsolute path to all libraries 
-# some are optionally, some may not exist on Linux
-#
-FIND_LIBRARY(OpenCV_CV_LIBRARY   
-  NAMES cv opencv
-  PATHS ${OpenCV_ROOT_DIR}  
-  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
-FIND_LIBRARY(OpenCV_CVAUX_LIBRARY
-  NAMES cvaux
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
-FIND_LIBRARY(OpenCV_CVCAM_LIBRARY   
-  NAMES cvcam
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES}) 
-FIND_LIBRARY(OpenCV_CVHAARTRAINING_LIBRARY
-  NAMES cvhaartraining
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES}) 
-FIND_LIBRARY(OpenCV_CXCORE_LIBRARY  
-  NAMES cxcore
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
-FIND_LIBRARY(OpenCV_CXTS_LIBRARY   
-  NAMES cxts
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
-FIND_LIBRARY(OpenCV_HIGHGUI_LIBRARY  
-  NAMES highgui
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
-FIND_LIBRARY(OpenCV_TRS_LIBRARY  
-  NAMES trs
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
+	#
+	# find sbsolute path to all libraries
+	# some are optionally, some may not exist on Linux
+	#
+	find_library(OPENCV_legacy_LIBRARY
+		NAMES
+		opencv_legacy
+		HINTS
+		"${OPENCV_BASE_DIR}"
+		"${OPENCV_ROOT_DIR}"
+		PATH_SUFFIXES
+		${OPENCV_LIBDIR_SUFFIXES})
+
+	set(OPENCV_NEW_COMPONENTS calib3d contrib core features2d highgui imgproc legacy ml objdetect video)
+	set(OPENCV_OLD_COMPONENTS cv cvaux cvcam cvhaartraining cxcore cxts highgui ml trs)
+	set(opencv_components)
+	if(OPENCV_NEW_LIBRARY_NAMES OR OPENCV_legacy_LIBRARY)
+
+		# New-style library names
+		foreach(component ${OPENCV_NEW_COMPONENTS})
+			find_library(OPENCV_${component}_LIBRARY
+			NAMES
+			opencv_${component}
+			HINTS
+			${OPENCV_BASE_DIR}
+			PATH_SUFFIXES
+			${OPENCV_LIBDIR_SUFFIXES})
+		endforeach()
+
+		# cv components with header and library if COMPONENTS unspecified
+		if(NOT OpenCV_FIND_COMPONENTS)
+			# default
+			set(opencv_components core legacy imgproc highgui)
+			if(WIN32)
+				list(APPEND opencv_components video)	# WIN32 only actually
+			endif()
+		else()
+			# TODO: clean up/convert to new components
+			string(TOLOWER "${OpenCV_FIND_COMPONENTS}" opencv_components)
+		endif()
+
+	else()
+		# Old-style lib names
+		if(NOT OpenCV_FIND_COMPONENTS)
+			# default
+			set(opencv_components cv cxcore cvaux highgui)
+			if(WIN32)
+				list(APPEND opencv_components cvcam)	# WIN32 only actually
+			endif()
+		else()
+			string(TOLOWER "${OpenCV_FIND_COMPONENTS}" opencv_components)
+		endif()
+
+		foreach(component ${OPENCV_OLD_COMPONENTS})
+			find_library(OPENCV_${component}_LIBRARY
+				NAMES
+				${component}
+				HINTS
+				${OPENCV_BASE_DIR}
+				PATH_SUFFIXES
+				${OPENCV_LIBDIR_SUFFIXES})
+		endforeach()
+	endif()
+
+	#
+	# Logic selecting required libs and headers
+	#
+
+	set(_req_check)
+	set(_req_libs)
+	set(_req_includes)
+	foreach(component ${opencv_components})
+		#message(STATUS "Component requested: ${component}")
+
+		# only good if header and library both found
+		list(APPEND
+			_req_check
+			OPENCV_${component}_LIBRARY)
+		list(APPEND _req_libs "${OPENCV_${component}_LIBRARY}")
+		if(DEFINED OPENCV_${component}_INCLUDE_DIR)
+			list(APPEND
+				_req_check
+				OPENCV_${component}_INCLUDE_DIR)
+			list(APPEND _req_includes "${OPENCV_${component}_INCLUDE_DIR}")
+		endif()
 
 
+	endforeach()
 
-FIND_LIBRARY(OpenCV_CORE_LIBRARY  
-  NAMES opencv_core
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
+	# handle the QUIETLY and REQUIRED arguments and set xxx_FOUND to TRUE if
+	# all listed variables are TRUE
+	include(FindPackageHandleStandardArgs)
+	find_package_handle_standard_args(OpenCV
+		DEFAULT_MSG
+		OPENCV_cv_INCLUDE_DIR
+		${_req_check})
 
-FIND_LIBRARY(OpenCV_HIGHGUI_LIBRARY  
-  NAMES opencv_highgui
-  PATHS ${OpenCV_ROOT_DIR}  PATH_SUFFIXES ${OpenCV_LIBDIR_SUFFIXES})
+	if(OPENCV_FOUND)
+		set(OPENCV_LIBRARY_DIRS)
+		foreach(lib ${_req_libs})
+			get_filename_component(dir "${lib}" PATH)
+			list(APPEND OPENCV_LIBRARY_DIRS "${dir}")
+		endforeach()
+		list(REVERSE OPENCV_LIBRARY_DIRS)
+		list(REMOVE_DUPLICATES OPENCV_LIBRARY_DIRS)
+		list(REVERSE OPENCV_LIBRARY_DIRS)
 
+		set(OPENCV_INCLUDE_DIRS ${_req_includes})
+		set(OPENCV_LIBRARIES ${_req_libs})
+		mark_as_advanced(OPENCV_ROOT_DIR OpenCV_DIR)
+	endif()
 
+	mark_as_advanced(OPENCV_BASE_DIR)
+	foreach(component ${OPENCV_NEW_COMPONENTS} ${OPENCV_OLD_COMPONENTS})
+		mark_as_advanced(OPENCV_${component}_LIBRARY OPENCV_${component}_INCLUDE_DIR)
+	endforeach()
+endif()
 
-# Logic selecting required libs and headers
-SET(OpenCV_FOUND ON)
-FOREACH(NAME ${OpenCV_FIND_REQUIRED_COMPONENTS})
-  # only good if header and library both found   
-  IF(OpenCV_${NAME}_INCLUDE_DIR AND OpenCV_${NAME}_LIBRARY)
-    LIST(APPEND OpenCV_INCLUDE_DIRS "${OpenCV_${NAME}_INCLUDE_DIR}")
-    LIST(APPEND OpenCV_LIBRARIES    "${OpenCV_${NAME}_LIBRARY}")
-  ELSE(OpenCV_${NAME}_INCLUDE_DIR AND OpenCV_${NAME}_LIBRARY)
-    message("Could not find OpenCV_${NAME}_INCLUDE_DIR")
-    SET(OpenCV_FOUND OFF)
-  ENDIF(OpenCV_${NAME}_INCLUDE_DIR AND OpenCV_${NAME}_LIBRARY)
-ENDFOREACH(NAME)
-
-message("Include dir: ${OpenCV_FOUND}")
-
-# get the link directory for rpath to be used with LINK_DIRECTORIES: 
-IF(OpenCV_CV_LIBRARY)
-  GET_FILENAME_COMPONENT(OpenCV_LINK_DIRECTORIES ${OpenCV_CV_LIBRARY} PATH)
-ENDIF(OpenCV_CV_LIBRARY)
-
-MARK_AS_ADVANCED(
-  OpenCV_ROOT_DIR
-  OpenCV_INCLUDE_DIRS
-  OpenCV_CV_INCLUDE_DIR
-  OpenCV_CXCORE_INCLUDE_DIR
-  OpenCV_CVAUX_INCLUDE_DIR
-  OpenCV_CVCAM_INCLUDE_DIR
-  OpenCV_HIGHGUI_INCLUDE_DIR
-  OpenCV_LIBRARIES
-  OpenCV_CV_LIBRARY
-  OpenCV_CXCORE_LIBRARY
-  OpenCV_CVAUX_LIBRARY
-  OpenCV_CVCAM_LIBRARY
-  OpenCV_CVHAARTRAINING_LIBRARY
-  OpenCV_CXTS_LIBRARY
-  OpenCV_HIGHGUI_LIBRARY
-  OpenCV_TRS_LIBRARY
- )
-
-
-# display help message
-IF(NOT OpenCV_FOUND)
-  # make FIND_PACKAGE friendly
-  IF(OpenCV_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR
-            "OpenCV required but some headers or libs not found. Please specify it's location with OpenCV_ROOT_DIR env. variable.")
-  ELSE(OpenCV_FIND_REQUIRED)
-    MESSAGE(STATUS 
-            "ERROR: OpenCV was not found.")
-  ENDIF(OpenCV_FIND_REQUIRED)
-ELSE(NOT OpenCV_FOUND)
-  MESSAGE(STATUS
-          "OpenCV found at ${OpenCV_ROOT_DIR}")
-ENDIF(NOT OpenCV_FOUND)
-
-
-#
-# $Log: FindOpenCV.cmake,v $
-# Revision 1.2  2008/11/03 16:40:34  vagvoba
-# cmake: Proper cvcam and highgui directories added to FindOpenCV.cmake
-#
-# Revision 1.1  2007/07/12 00:22:25  anton
-# cmake utilities: Added FindOpenCV.cmake based on example found all over the
-# web.  Will have to be removed when CMake provides one.
-#
-#
 
--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
 #include "common.h"
 #include <thread>
 #include "CEventReceiver.hpp"
-#include "IQuadSceneNode.h"
 
 using namespace irr;
 
@@ -28,61 +27,67 @@ CEventReceiver receiver;
 scene::ISceneManager* smgr;
 scene::ICameraSceneNode* cam;
 cv::VideoCapture videocapture;
-cv::Mat camimg, camimgund;
+cv::Mat camframe, camframeund;
 bool is_running = true;
 video::ITexture* tex;
 int ww, wh;
 
-void InitIrrlicht( int wWidth, int wHeight)
-{
-	device = createDevice(
-			video::EDT_OPENGL, core::dimension2d<u32>(wWidth, wHeight),
-			16, false, false, false, &receiver);
-	if(!device)
-		exit(EXIT_FAILURE);
-
-	driver = device->getVideoDriver();
-	smgr = device->getSceneManager();
-	receiver.SetIrrDevice(device);
-}
-
-
-void SpawnBall( float x, float y, float z, float radius )
-{
+void SpawnBall( float x, float y, float z, float radius ){
 	scene::ISceneNode* node = smgr->addSphereSceneNode(radius);
 	node->setPosition( core::vector3df( x, y, z ) );
 	node->setMaterialFlag(video::EMF_LIGHTING,0);
 	node->setMaterialFlag(video::EMF_NORMALIZE_NORMALS, true);
 }
 
-void UpdateRenderer()
-{
-	if(device->isWindowActive())
-	{
-		/* RENDERING */
-		driver->beginScene(true, true, video::SColor(64, 67, 74, 255));
-		driver->draw2DImage( tex, core::position2d<s32>(ww/4,wh/4) );
-		smgr->drawAll();
-		driver->endScene();
-	}
-	else
-	{
-		device->yield();
-	}
-}
-
-void CameraLoop()
-{
+void CameraLoop() {
 	while( videocapture.grab() && is_running )
 	{
-		videocapture.retrieve (camimg);
-		cv::imshow("in", camimg);
-		cv::waitKey(1);
+		videocapture.retrieve (camframe);
+		//cv::imshow("in", camframe);
+		//cv::waitKey(1);
 	}
 }
 
-void SetupScene()
-{
+std::string cvmattype2str(int type) {
+	std::string r;
+
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+	switch ( depth ) {
+		case CV_8U:  r = "8U"; break;
+		case CV_8S:  r = "8S"; break;
+		case CV_16U: r = "16U"; break;
+		case CV_16S: r = "16S"; break;
+		case CV_32S: r = "32S"; break;
+		case CV_32F: r = "32F"; break;
+		case CV_64F: r = "64F"; break;
+		default:     r = "User"; break;
+	}
+
+	r += "C";
+	r += (chans+'0');
+
+	return r;
+}
+
+
+std::string irrtextype2str( video::ECOLOR_FORMAT fmt ) {
+	switch( fmt ) {
+		case video::ECF_A1R5G5B5: return "ECF_A1R5G5B5";
+		case video::ECF_R5G6B5: return "ECF_R5G6B5";
+		case video::ECF_R8G8B8: return "ECF_R8G8B8";
+		case video::ECF_A8R8G8B8: return "ECF_A8R8G8B8";
+		case video::ECF_R16F: return "ECF_R16F";
+		case video::ECF_G16R16F: return "ECF_G16R16F";
+		case video::ECF_A16B16G16R16F: return "ECF_A16B16G16R16F";
+		case video::ECF_R32F: return "ECF_R32F";
+		case video::ECF_G32R32F: return "ECF_G32R32F";
+		case video::ECF_A32B32G32R32F: return "ECF_A32B32G32R32F";
+	}
+}
+
+void SetupScene() {
 	SKeyMap keyMap[8];
 	keyMap[0].Action = EKA_MOVE_FORWARD;
 	keyMap[0].KeyCode = KEY_UP;
@@ -120,22 +125,56 @@ int main ()
 	}
 
 	// get test image to know size
-	videocapture >> camimg;
+	videocapture >> camframe;
+	ww = camframe.cols;
+	wh = camframe.rows;
 
-	// set width and height
-	ww = camimg.cols;
-	wh = camimg.rows;
-
-	InitIrrlicht(ww, wh);
+	// init irrlicht stuff
+	device = createDevice( video::EDT_OPENGL, core::dimension2d<u32>(ww, wh), 16, false, false, false, &receiver );
+	if(!device) exit(EXIT_FAILURE);
+	driver = device->getVideoDriver();
+	smgr = device->getSceneManager();
+	receiver.SetIrrDevice(device);
 	SetupScene();
 
-	tex = driver->addTexture(core::dimension2d<u32>(ww/2, wh/2), "asd");
+	// init irrlicht texture
+	video::IImage* img = driver->createImage(video::ECOLOR_FORMAT::ECF_R8G8B8, core::dimension2d<u32>(ww,wh));
+	tex = driver->addTexture("asd", img);
+
+	printf("OpenCV camera image: %s %dx%d \n", cvmattype2str( camframe.type() ).c_str(), camframe.cols, camframe.rows );
+	printf("Irrlicht texture: %s, [%d,%d]\n", irrtextype2str(tex->getColorFormat()).c_str(), tex->getSize().Width, tex->getSize().Height );
 
 	// launch cam grabbing loop thread
 	std::thread camthread( CameraLoop );
 
 	// main rendering loop
-	while( device->run() ) UpdateRenderer();
+	while( device->run() )
+	{
+		if(device->isWindowActive())
+		{
+			/* RENDERING */
+			//cv::Mat aux( camframe.rows, camframe.cols, CV_8UC4, camframe.data );
+			//cv::Mat aux2;
+			//aux.copyTo(aux2);
+
+			cv::Mat RGBA(camframe.size(), CV_8UC4);
+			cv::cvtColor(camframe, RGBA, CV_RGB2RGBA, 4);
+
+			void* texdata = tex->lock();
+			unsigned char* camdata = RGBA.data;
+			memcpy(texdata, camdata, camframe.cols * camframe.rows * sizeof(uint8_t) * 4);
+			tex->unlock();
+
+			driver->beginScene(true, true, video::SColor(64, 67, 74, 255));
+			driver->draw2DImage( tex, core::position2d<s32>(0,0) );
+			smgr->drawAll();
+			driver->endScene();
+		}
+		else
+		{
+			device->yield();
+		}
+	}
 
 	// tell cam loop to finish and wait for it
 	is_running = false;
